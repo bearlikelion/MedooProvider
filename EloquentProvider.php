87ec682a31f825853083a7b251a3c99a119cbd4d
Mark Arneman <mark@purzue.com>
2014-09-18 15:42:21 -0400

    Syntax Error

--- EloquentProvider.php
+++ EloquentProvider.php
@@ -15,7 +15,7 @@ class EloquentProvider extends ServiceProvider
 	{
 		$this->app->share('Illuminate\Database\Capsule\Manager', function() {
 		    $capsule = new Capsule;
-		    $connection = $this->app->getConfig()->get("database.connection")
+		    $connection = $this->app->getConfig()->get("database.connection");
 		    $capsule->addConnection($this->app->getConfig()->get("database.connections.$connection");
 		    return $capsule;
 		});
