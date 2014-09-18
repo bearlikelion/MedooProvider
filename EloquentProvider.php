b136439ebe5fc1700ee09c7102be0dedb497e9b1
Mark Arneman <mark@purzue.com>
2014-09-18 15:38:41 -0400

    Eloquent Provider

---
+++ EloquentProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace Bearlikelion;
+
+use Illuminate\Database\Capsule\Manager;
+
+use Autarky\Kernel\ServiceProvider;
+use Autarky\Container\ContainerInterface;
+
+/**
+ * Service provider for laravel's Eloquent ORM
+ */
+class EloquentProvider extends ServiceProvider
+{
+	public function register()
+	{
+		$this->app->share('Illuminate\Database\Capsule\Manager', function() {
+		    $capsule = new Capsule;
+		    $connection = $this->app->getConfig()->get("database.connection")
+		    $capsule->addConnection($this->app->getConfig()->get("database.connections.$connection");
+		    return $capsule;
+		});
+
+	    $this->app->config(function($app) {
+	        $this->app->resolve('Illuminate\Database\Capsule\Manager')->bootEloquent();
+	    });
+	}
+}
